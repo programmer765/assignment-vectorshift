9533101ede558dea640a673d2985aaf0eeddfa58
Aviprit Shah <avipritshah15@gmail.com>
2025-10-18 22:15:01 +0530

    completed first part of assignment

--- backend/integrations/hubspot.py
+++ backend/integrations/hubspot.py
@@ -30,8 +30,60 @@ async def authorize_hubspot(user_id, org_id):
     return f'{authorization_url}&state={encoded_state}'
 
 async def oauth2callback_hubspot(request: Request):
-    # TODO
-    pass
+    if request.query_params.get('error'):
+        raise HTTPException(status_code=400, detail=request.query_params.get('error'))
+    code = request.query_params.get('code')
+    encoded_state = request.query_params.get('state')
+    if not code or not encoded_state:
+        raise HTTPException(status_code=400, detail='Missing code or state in the callback.')
+    state_data = json.loads(encoded_state)
+
+    original_state = state_data.get('state')
+    user_id = state_data.get('user_id')
+    org_id = state_data.get('org_id')
+
+    saved_state_json = await get_value_redis(f'hubspot_state:{org_id}:{user_id}')
+    
+    saved_state_obj = json.loads(saved_state_json)
+    saved_state = saved_state_obj.get('state')
+    print("Original State:", original_state)
+    print("Saved State:", saved_state)
+    
+    if not saved_state or saved_state != original_state:
+        raise HTTPException(status_code=400, detail='State does not match.')
+
+    # Exchange the authorization code for an access token
+    async with httpx.AsyncClient() as client:
+        response, _ = await asyncio.gather(
+            client.post(
+                'https://api.hubapi.com/oauth/v1/token', 
+                data={
+                    'grant_type': 'authorization_code',
+                    'client_id': CLIENT_ID,
+                    'client_secret': CLIENT_SECRET,
+                    'redirect_uri': REDIRECT_URI,
+                    'code': code
+                },
+                headers={'Content-Type': 'application/x-www-form-urlencoded'}
+            ),
+            delete_key_redis(f'hubspot_state:{org_id}:{user_id}')
+        )
+    response_data = response.json()
+    # if response.status_code != 200:
+    #     raise HTTPException(status_code=400, detail=response_data.get('error_description'))
+
+    # Save the response in Redis
+    await add_key_value_redis(f'hubspot_credentials:{org_id}:{user_id}', json.dumps(response_data), expire=600)
+
+    close_window_script = """
+    <html>
+        <script>
+            window.close();
+        </script>
+    </html>
+    """
+
+    return HTMLResponse(content=close_window_script)
 
 async def get_hubspot_credentials(user_id, org_id):
     credentials = await get_value_redis(f'hubspot_credentials:{org_id}:{user_id}')
@@ -44,10 +96,16 @@ async def get_hubspot_credentials(user_id, org_id):
 
     return credentials
 
-async def create_integration_item_metadata_object(response_json):
-    # TODO
-    pass
+# def recurs
+
+async def create_integration_item_metadata_object(
+    response_json_str: str
+):
+    response_data=json.loads(json.dumps(response_json_str))
+    
+
+    # return integration_item_metadata
 
 async def get_items_hubspot(credentials):
-    # TODO
+    """Aggregate all metadata relevant for a HubSpot data fetching logic here"""
     pass
